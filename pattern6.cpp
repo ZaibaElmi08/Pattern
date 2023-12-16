ba0cc82d0680b59292ee6d9b5e2c2cd50a0161e3
ZaibaElmi08 <96732111+ZaibaElmi08@users.noreply.github.com>
2023-12-16 16:52:00 +0530

    Add files via upload

---
+++ pattern6.cpp
@@ -0,0 +1,18 @@
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+#include<iostream>
+using namespace std;
+int main()
+{
+  for(int row=0;row<5;row++)
+  {
+    for(int col=0;col<row+1;col++)
+    {
+      cout<<col+1<<" ";
+    }
+    cout<<endl;
+  }
+}
